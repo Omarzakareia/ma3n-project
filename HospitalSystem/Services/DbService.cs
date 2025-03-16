9ce2b1662aabec520d27429438f8a2ace57473e2
ZeyadMashhour608 <zeyadmashhour12345@gmail.com>
2025-03-16 15:18:25 -0700

    Created Services Folder
    Services Folder Created Added Database Models to the projected added them to gitignore file created singelton dbcontext in as services file to be used by the rest of the system.
    Made sure it works
    
    Delete web.config

---
+++ HospitalSystem/Services/DbService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace HospitalSystem.Services
+{
+	public class DbService
+	{
+		private static DbService _instance;
+		public readonly string _connectionString;
+
+		private DbService()
+		{
+            var conn = ConfigurationManager.ConnectionStrings["MyDB"]; 
+            if (conn == null)
+            {
+                throw new Exception("❌ Connection string 'MyDB' not found in web.config!");
+            }
+            _connectionString = conn.ConnectionString;
+        }
+		public static DbService Instance
+		{
+			get
+			{
+				if(_instance == null )
+				{
+					_instance = new DbService();
+				}
+				return _instance;
+			}
+		}
+        public HospitalEntities GetDbContext()
+        {
+            return new HospitalEntities(); 
+        }
+    }
+
+}
