ea0559f2384d47514d0f44a37f4c4d2fabaa10aa
AhmedMM1122 <130315056+AhmedMM1122@users.noreply.github.com>
2025-03-16 13:13:40 +0200

    Create Departments

---
+++ HospitalSystem/Doctors/Departments.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Departments.aspx.cs" Inherits="HospitalSystem.Doctors.Departments" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <telerik:RadGrid ID="RadGrid1" runat="server">
+    </telerik:RadGrid>
+</asp:Content>
+
--- HospitalSystem/MasterPage.Master.designer.cs
+++ HospitalSystem/MasterPage.Master.designer.cs
@@ -24,6 +24,33 @@ namespace HospitalSystem
 		protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
 		/// <summary>
+		/// form1 control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+		/// <summary>
+		/// RadScriptManager1 control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::Telerik.Web.UI.RadScriptManager RadScriptManager1;
+
+		/// <summary>
+		/// RadAjaxManager1 control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::Telerik.Web.UI.RadAjaxManager RadAjaxManager1;
+
+		/// <summary>
 		/// ContentPlaceHolder1 control.
 		/// </summary>
 		/// <remarks>
