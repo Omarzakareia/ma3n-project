23e7d840d98870527e7ade896959b20b32a9a524
AhmedMM1122 <130315056+AhmedMM1122@users.noreply.github.com>
2025-03-17 15:07:02 +0200

    Show Doctors assigned to Departments

--- HospitalSystem/Doctors/Departments.aspx.cs
+++ HospitalSystem/Doctors/Departments.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Web.UI;
 
 namespace HospitalSystem.Doctors
 {
@@ -13,5 +16,46 @@ namespace HospitalSystem.Doctors
 		{
 
 		}
+
+        protected void RadGrid1_NeedDataSource(object sender, Telerik.Web.UI.GridNeedDataSourceEventArgs e)
+        {
+
+        }
+
+		protected void SmallHospital_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+		{
+
+		}
+		protected void RadGrid1_ItemDataBound(object sender, GridItemEventArgs e)
+		{
+			if (e.Item is GridDataItem)
+			{
+				GridDataItem item = (GridDataItem)e.Item;
+				DropDownList ddlDoctors = (DropDownList)item.FindControl("ddlDoctors");
+
+				if (ddlDoctors != null)
+				{
+					string connStr = ConfigurationManager.ConnectionStrings["InternSmallHospitalConnectionString"].ConnectionString;
+					using (SqlConnection conn = new SqlConnection(connStr))
+					{
+						conn.Open();
+						string query = "SELECT d.DoctorID, u.FullName AS DoctorName FROM Doctor d INNER JOIN [User] u ON d.UserID = u.UserID WHERE d.DepartmentID IS NULL OR d.DepartmentID = 0";
+						SqlCommand cmd = new SqlCommand(query, conn);
+						SqlDataReader reader = cmd.ExecuteReader();
+
+						ddlDoctors.DataSource = reader;
+						ddlDoctors.DataTextField = "DoctorName";
+						ddlDoctors.DataValueField = "DoctorID";
+						ddlDoctors.DataBind();
+						reader.Close();
+					}
+
+					// Add default "Select Doctor" item
+					ddlDoctors.Items.Insert(0, new ListItem("Select Doctor", ""));
+				}
+			}
+		}
+
+
 	}
 }
--- HospitalSystem/Doctors/Departments.aspx.designer.cs
+++ HospitalSystem/Doctors/Departments.aspx.designer.cs
@@ -22,5 +22,32 @@ namespace HospitalSystem.Doctors
 		/// To modify move field declaration from designer file to code-behind file.
 		/// </remarks>
 		protected global::Telerik.Web.UI.RadGrid RadGrid1;
+
+		/// <summary>
+		/// Doctors control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.SqlDataSource Doctors;
+
+		/// <summary>
+		/// DoctorsList control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.SqlDataSource DoctorsList;
+
+		/// <summary>
+		/// SmallHospital control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.SqlDataSource SmallHospital;
 	}
 }
