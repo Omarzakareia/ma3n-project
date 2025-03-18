a083c0708b4ee3a8ab27b6bfa66514154c44fb24
ZeyadMashhour <zeyadmashhour12345@gmail.com>
2025-03-18 11:03:53 +0200

    Added Delete Functionality

--- HospitalSystem/Patients/Patients.aspx.cs
+++ HospitalSystem/Patients/Patients.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using HospitalSystem.Services;
+using Telerik.Web.UI;
 
 namespace HospitalSystem.Patients
 {
@@ -46,6 +47,66 @@ namespace HospitalSystem.Patients
                 RadGrid1.DataBind();
             }
         }
+        protected void RadGrid1_ItemCommand(object sender, GridCommandEventArgs e)
+        {
+            Response.Write("<script>alert('Delete function triggered!');</script>"); // 🔹 Debugging step
 
+            if (e.CommandName == "DeletePatient")
+            {
+                GridDataItem item = e.Item as GridDataItem;
+                if (item != null)
+                {
+                    int patientId = Convert.ToInt32(item.GetDataKeyValue("PatientID"));
+                    Response.Write("<script>alert('Patient ID: " + patientId + "');</script>"); // 🔹 Debugging step
+
+                    using (var db = DbService.Instance.GetDbContext())
+                    {
+                        var patient = db.Patients.FirstOrDefault(p => p.PatientID == patientId);
+                        if (patient != null)
+                        {
+                            patient.IsDeleted = true;
+                            db.SaveChanges();
+
+                            Response.Write("<script>alert('Marked as deleted in DB!');</script>"); // 🔹 Debugging step
+
+                            LoadPatientData();
+                        }
+                        else
+                        {
+                            Response.Write("<script>alert('Error: Patient not found!');</script>");
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void RadGrid1_DeleteCommand(object sender, GridCommandEventArgs e)
+        {
+            GridDataItem item = e.Item as GridDataItem;
+            if (item != null)
+            {
+                int patientId = Convert.ToInt32(item.GetDataKeyValue("PatientID"));
+                Response.Write("<script>alert('Patient ID: " + patientId + "');</script>"); // 🔹 Debugging step
+
+                using (var db = DbService.Instance.GetDbContext())
+                {
+                    var patient = db.Patients.FirstOrDefault(p => p.PatientID == patientId);
+                    if (patient != null)
+                    {
+                        patient.IsDeleted = true;
+                        patient.DeletedAt = DateTime.Now;
+                        db.SaveChanges();
+
+                        Response.Write("<script>alert('Marked as deleted in DB!');</script>"); // 🔹 Debugging step
+
+                        LoadPatientData();
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('Error: Patient not found!');</script>");
+                    }
+                }
+            }
+        }
     }
 }
